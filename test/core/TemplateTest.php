1791e85f57520a67d8b88ac84be2d084ef48c847
Chris Johnson <tenorviol@yahoo.com>
2012-10-13 06:58:00 -0700

    Imported Template class from zeroclass lib

---
+++ test/core/TemplateTest.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'lib/zero/core/Template.php';
+
+use zero\core\Template;
+
+class TemplateTest extends PHPUnit_Framework_TestCase {
+
+	public function testDisplay() {
+		$template = new Template('test/template/foo.php');
+		$template->class = "red";
+
+		ob_start();
+		$template->display();
+		$output = ob_get_contents();
+		ob_end_clean();
+
+		$this->assertEquals('<div class="red">foo</div>'."\n", $output);
+	}
+}
